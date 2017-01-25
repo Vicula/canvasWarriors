ab9664aff047217e5c672843c9ec314ff5859556
Victor Carpenter <victorcarpenter123@gmail.com>
2017-01-25 10:33:56 -0500

    learing react router

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,17 @@
 // console.log('Hello World!');
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Router from 'react-router';
+import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+import Home from './pages/home'
+import Landing from './pages/landing'
+
+const app = document.getElementById('mount');
 
 
-console.log(Router)
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={Landing}>
+      <IndexRoute component={Home}></IndexRoute>
+    </Route>
+  </Router>,
+app)
