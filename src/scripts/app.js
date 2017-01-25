f1517adee4e3b1840398520632ed2727dabf2ea4
Victor Carpenter <victorcarpenter123@gmail.com>
2017-01-25 09:46:01 -0500

    added react router

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,11 +1,7 @@
 // console.log('Hello World!');
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './Counter';
+import Router from 'react-router';
 
-document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(
-    React.createElement(Counter),
-    document.getElementById('mount')
-  );
-});
+
+console.log(Router)
